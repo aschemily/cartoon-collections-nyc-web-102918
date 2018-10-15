4337f9e08df6377be2aa8691db3dcd16757684ac
Emily Asch <e.asch@me.com>
2018-10-15 17:27:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarfs) # code an argument here
   # Your code here
   dwarfs.each_with_index do |name, index|
     
-    puts "#{(index + 1) #{name}"
+    puts "#{index + 1, #{name}"
   end
 end
 
