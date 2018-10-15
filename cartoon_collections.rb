d9c0876843ca11ac820770d83aea25474ae666f0
Emily Asch <e.asch@me.com>
2018-10-15 17:14:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,10 @@ def long_planeteer_calls(fruits)# code an argument here
 end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheddar_cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0 
+  while i < cheese_types.length
+  return cheese_types[i] if array.include?()
 end
