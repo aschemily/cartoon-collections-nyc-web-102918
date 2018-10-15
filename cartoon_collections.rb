586581f87ca0a57fa14fe8a4015c8a8119c7366f
Emily Asch <e.asch@me.com>
2018-10-15 17:19:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,8 @@ def find_the_cheese(cheddar_cheese)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   while i < cheese_types.length
-  return cheese_types[i] if cheddar_cheese.include?(cheese_types[i])
+  if cheddar_cheese.include?(cheese_types[i])
   i += 1 
+  return cheese_types[i] 
   end
 end
