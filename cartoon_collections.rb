771e1890cfe531a847c7220a0f1652d8483d4941
Emily Asch <e.asch@me.com>
2018-10-15 17:23:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,19 +13,19 @@ def summon_captain_planet(veggies)# code an argument here
   end
 end
 
-def long_planeteer_calls(fruits)# code an argument here
-  # Your code here
-  fruits.any? do |call|
-  call.length > 4
-  end
-end
+# def long_planeteer_calls(fruits)# code an argument here
+#   # Your code here
+#   fruits.any? do |call|
+#   call.length > 4
+#   end
+# end
 
-def find_the_cheese(array)# code an argument here
-  # the array below is here to help
+# def find_the_cheese(array)# code an argument here
+#   # the array below is here to help
  
-  cheese_types = ["cheddar", "gouda", "camembert"]
+#   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.find do |type|
-  cheese_types.include?(type)
-end
-end
+#   array.find do |type|
+#   cheese_types.include?(type)
+# end
+# end
