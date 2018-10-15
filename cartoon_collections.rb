869ff01823fabf3f73519c5794dbd3563b54f910
Emily Asch <e.asch@me.com>
2018-10-15 17:27:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,12 +13,12 @@ def summon_captain_planet(veggies)# code an argument here
   end
 end
 
-# def long_planeteer_calls(fruits)# code an argument here
-#   # Your code here
-#   fruits.any? do |call|
-#   call.length > 4
-#   end
-# end
+def long_planeteer_calls(fruits)# code an argument here
+  # Your code here
+  fruits.any? do |call|
+  call.length > 4
+  end
+end
 
 # def find_the_cheese(array)# code an argument here
 #   # the array below is here to help
