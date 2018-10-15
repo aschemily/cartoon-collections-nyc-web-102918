bca5d8fdd53f0f4944527415e9eac6fbc86dd4fb
Emily Asch <e.asch@me.com>
2018-10-15 17:15:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,14 +10,14 @@ def summon_captain_planet(veggies)# code an argument here
   # Your code here
   veggies.collect do |call|
   call.capitalize << "!"
-end
+  end
 end
 
 def long_planeteer_calls(fruits)# code an argument here
   # Your code here
   fruits.any? do |call|
   call.length > 4
-end
+  end
 end
 
 def find_the_cheese(cheddar_cheese)# code an argument here
@@ -27,5 +27,5 @@ def find_the_cheese(cheddar_cheese)# code an argument here
   while i < cheese_types.length
   return cheese_types[i] if array.include?(cheese_types[i])
   i += 1 
-end
+  end
 end
